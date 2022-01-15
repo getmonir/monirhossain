62e96be431594df4fc6438b95897dbcf49f04fdd
Monir Hossain <getmonirhossain@gmail.com>
2022-01-16 00:39:37 +0600

    Spelling correct

--- components/Hero.js
+++ components/Hero.js
@@ -8,7 +8,7 @@ const Hero = () => {
         <div className='row align-items-center'>
           <div className='col-lg-8 mx-auto'>
             <div className='pt-5 pb-10 text-center'>
-              <h1 className='display-5'>Freelnace WordPress Developer</h1>
+              <h1 className='display-5'>Freelance WordPress Developer</h1>
               <p className='lead py-3'>
                 Freelance specialist in responsive WordPress design, front-end
                 design, custom WordPress builds and WooCommerce.
