230c9325eaa12f03ed312b695e6c252f05102461
Monir Hossain <getmonirhossain@gmail.com>
2022-01-28 15:02:01 +0600

    Add Tage

--- pages/_app.js
+++ pages/_app.js
@@ -1,26 +1,24 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import Layout from '../components/Layout';
 import '../styles/style.css';
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      window.gtag('config', process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS, {
+        page_path: url,
+      });
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <>
-      <script
-        async
-        src='https://www.googletagmanager.com/gtag/js?id=G-781TZL953Q'
-      ></script>
-      <script>
-        dangerouslySetInnerHTML=
-        {{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', 'G-781TZL953Q');
-            });
-          `,
-        }}
-      </script>
-
       <Layout>
         <Component {...pageProps} />
       </Layout>
