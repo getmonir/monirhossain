7211db9e08e1fc0efb2e8adff1f158955cbfb22c
Monir Hossain <getmonirhossain@gmail.com>
2022-01-28 14:19:30 +0600

    Add Analytices

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,28 @@ import '../styles/style.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <script
+        async
+        src='https://www.googletagmanager.com/gtag/js?id=G-781TZL953Q'
+      ></script>
+      <script>
+        dangerouslySetInnerHTML=
+        {{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-781TZL953Q');
+            });
+          `,
+        }}
+      </script>
+
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
