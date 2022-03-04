b47bd3990a40962daa54eb26324e111809286dee
Monir Hossain <getmonirhossain@gmail.com>
2022-03-04 22:09:05 +0600

    Fix issues

--- components/Hero.js
+++ components/Hero.js
@@ -4,7 +4,7 @@ const Hero = () => {
       <div className='container'>
         <div className='row align-items-center'>
           <div className='col-lg-8 mx-auto text-center'>
-              <h2 className='display-5 mb-3'>Hi, I'm Monir <br />
+              <h2 className='display-5 mb-3'>{`Hi, I'm Monir`} <br />
               Welcome to my portfolio.</h2>
               <p className='lead'>
                 {`I enjoy sharing the projects just as much as I enjoy creating them. Sit back & take a moment to browse through some of my completed work.`}
