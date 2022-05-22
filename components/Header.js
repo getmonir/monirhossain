e77d5951a72881f4f351da0a61461e6031f5e055
Monir Hossain <getmonirhossain@gmail.com>
2022-05-22 22:40:15 +0600

    Update links

--- components/Header.js
+++ components/Header.js
@@ -1,66 +1,64 @@
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import {
-  RiCloseCircleLine,
-  RiMenu2Fill,
-  RiLinkedinBoxFill,
-  RiTwitterFill,
-  RiGithubFill,
-  RiInstagramLine,
-  RiFacebookCircleFill,
+	RiCloseCircleLine,
+	RiMenu2Fill,
+	RiLinkedinBoxFill,
+	RiTwitterFill,
+	RiGithubFill,
+	RiInstagramLine,
+	RiFacebookCircleFill,
 } from 'react-icons/ri';
 const Header = () => {
-  const [stickyClass, setStickyClass] = useState('relative');
-  useEffect(() => {
-    window.addEventListener('scroll', stickNavbar);
+	const [stickyClass, setStickyClass] = useState('relative');
+	useEffect(() => {
+		window.addEventListener('scroll', stickNavbar);
 
-    return () => {
-      window.removeEventListener('scroll', stickNavbar);
-    };
-  }, []);
+		return () => {
+			window.removeEventListener('scroll', stickNavbar);
+		};
+	}, []);
 
-  const stickNavbar = () => {
-    if (window !== undefined) {
-      let windowHeight = window.scrollY;
-      windowHeight > 80
-        ? setStickyClass('fixed-header')
-        : setStickyClass('relative');
-    }
-  };
-  const [click, setClick] = useState(false);
-  const handleClick = () => setClick(!click);
-  const closeMobileMenu = () => setClick(false);
-  return (
-    <header className={`header ${stickyClass}`}>
-      <div className='container'>
-        <div className='navbar'>
-          <div className='navbar__logo'>
-            <Link href='/'>Monir Hossain</Link>
-          </div>
-          <ul className={click ? 'navbar__nav active' : 'navbar__nav'}>
-            <div className='mobile-logo' onClick={closeMobileMenu}>
-              <Link href='/'>
-                <a>Monir Hossain</a>
-              </Link>
-            </div>
+	const stickNavbar = () => {
+		if (window !== undefined) {
+			let windowHeight = window.scrollY;
+			windowHeight > 80 ? setStickyClass('fixed-header') : setStickyClass('relative');
+		}
+	};
+	const [click, setClick] = useState(false);
+	const handleClick = () => setClick(!click);
+	const closeMobileMenu = () => setClick(false);
+	return (
+		<header className={`header ${stickyClass}`}>
+			<div className='container'>
+				<div className='navbar'>
+					<div className='navbar__logo'>
+						<Link href='/'>Monir Hossain</Link>
+					</div>
+					<ul className={click ? 'navbar__nav active' : 'navbar__nav'}>
+						<div className='mobile-logo' onClick={closeMobileMenu}>
+							<Link href='/'>
+								<a>Monir Hossain</a>
+							</Link>
+						</div>
 
-            <li className='option' onClick={closeMobileMenu}>
-              <Link href='/about'>About</Link>
-            </li>
+						<li className='option' onClick={closeMobileMenu}>
+							<Link href='/about'>About</Link>
+						</li>
 
-            <li className='option' onClick={closeMobileMenu}>
-              <Link href='/website-templates'>Free Website Templates </Link>
-            </li>
+						<li className='option' onClick={closeMobileMenu}>
+							<Link href='/website-templates'>Free Website Templates </Link>
+						</li>
 
-            <li className='option' onClick={closeMobileMenu}>
+						{/* <li className='option' onClick={closeMobileMenu}>
               <Link href='/blog'>Blog</Link>
-            </li>
+            </li> */}
 
-            <li className='option' onClick={closeMobileMenu}>
-              <Link href='/contact'>Contact</Link>
-            </li>
+						<li className='option' onClick={closeMobileMenu}>
+							<Link href='/contact'>Contact</Link>
+						</li>
 
-            {/* <li className='option btn-fbg' onClick={closeMobileMenu}>
+						{/* <li className='option btn-fbg' onClick={closeMobileMenu}>
               <a
                 target='_blank'
                 href='https://facebook.com/groups/monirhossain/'
@@ -70,71 +68,45 @@ const Header = () => {
               </a>
             </li> */}
 
-            <div className='header-bottom'>
-              <ul>
-                <li>
-                  <a
-                    target='_blank'
-                    href='https://twitter.com/getmonir/'
-                    rel='noreferrer'
-                  >
-                    <RiTwitterFill />
-                  </a>
-                </li>
-                <li>
-                  <a
-                    target='_blank'
-                    href='https://facebook.com/getmonir/'
-                    rel='noreferrer'
-                  >
-                    <RiFacebookCircleFill />
-                  </a>
-                </li>
-                <li>
-                  <a
-                    target='_blank'
-                    href='https://instagram.com/getmonir/'
-                    rel='noreferrer'
-                  >
-                    <RiInstagramLine />
-                  </a>
-                </li>
-                <li>
-                  <a
-                    target='_blank'
-                    href='https://linkedin.com/in/getmonirh/'
-                    rel='noreferrer'
-                  >
-                    <RiLinkedinBoxFill />
-                  </a>
-                </li>
-                <li>
-                  <a
-                    target='_blank'
-                    href='https://github.com/getmonir/'
-                    rel='noreferrer'
-                  >
-                    <RiGithubFill />
-                  </a>
-                </li>
-              </ul>
-            </div>
-          </ul>
+						<div className='header-bottom'>
+							<ul>
+								<li>
+									<a target='_blank' href='https://twitter.com/getmonir/' rel='noreferrer'>
+										<RiTwitterFill />
+									</a>
+								</li>
+								<li>
+									<a target='_blank' href='https://facebook.com/getmonir/' rel='noreferrer'>
+										<RiFacebookCircleFill />
+									</a>
+								</li>
+								<li>
+									<a target='_blank' href='https://instagram.com/getmonir/' rel='noreferrer'>
+										<RiInstagramLine />
+									</a>
+								</li>
+								<li>
+									<a target='_blank' href='https://linkedin.com/in/getmonirh/' rel='noreferrer'>
+										<RiLinkedinBoxFill />
+									</a>
+								</li>
+								<li>
+									<a target='_blank' href='https://github.com/getmonir/' rel='noreferrer'>
+										<RiGithubFill />
+									</a>
+								</li>
+							</ul>
+						</div>
+					</ul>
 
-          <div
-            className={click ? 'navbar__toggler active' : 'navbar__toggler'}
-            onClick={handleClick}
-          >
-            {click ? <RiCloseCircleLine /> : <RiMenu2Fill />}
-          </div>
-        </div>
-      </div>
-      <div
-        className={click ? 'mobile-body-overly active' : 'mobile-body-overly'}
-        onClick={closeMobileMenu}
-      ></div>
-    </header>
-  );
+					<div className={click ? 'navbar__toggler active' : 'navbar__toggler'} onClick={handleClick}>
+						{click ? <RiCloseCircleLine /> : <RiMenu2Fill />}
+					</div>
+				</div>
+			</div>
+			<div className={click ? 'mobile-body-overly active' : 'mobile-body-overly'} onClick={closeMobileMenu}></div>
+		</header>
+	);
 };
 
 export default Header;
--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,107 +1,99 @@
 import Meta from '../../components/Meta';
-import {
-  RiMailSendLine,
-  RiMessengerFill,
-  RiSkypeLine,
-  RiWhatsappLine,
-} from 'react-icons/ri';
+import { RiMailSendLine, RiMessengerFill, RiSkypeLine, RiWhatsappLine } from 'react-icons/ri';
 const index = () => {
-  return (
-    <>
-      <Meta title='Contact Me - Monir Hossain' />
-      <header className='py-10 py-lg-11'>
-        <div className='container'>
-          <div className='row'>
-            <div className='col-lg-7 text-center mx-auto'>
-              <h2 className='display-5 mb-3'>Contact Me</h2>
-              <p className='mb-0 lead'>
-                I &apos;d love to find out more about your upcoming WordPress
-                project. Get in touch today.
-              </p>
-            </div>
-          </div>
-        </div>
-      </header>
-      <section className='pb-10 pb-lg-11'>
-        <div className='container'>
-          <div className='row'>
-            <div className='col-lg-4'>
-              <div className='border p-4 rounded-3 mb-4'>
-                <span className='fs-1'>
-                  <RiMailSendLine />
-                </span>
-                <h4>Email</h4>
-                <p className='mb-0 lead'>
-                  <a
-                    className='text-decoration-none text-link link-primary'
-                    href='mailto:monir@monirhossain.com'
-                    target='_blank'
-                    rel='noreferrer'
-                  >
-                    monir@monirhossain.com
-                  </a>
-                </p>
-              </div>
-            </div>
-            <div className='col-lg-4'>
-              <div className='border p-4 rounded-3 mb-4'>
-                <span className='fs-1'>
-                  <RiWhatsappLine />
-                </span>
-                <h4>Whatsapp</h4>
-                <p className='mb-0 lead'>
-                  <a
-                    className='text-decoration-none text-link link-primary'
-                    href='https://wa.me/+8801710021512'
-                    target='_blank'
-                    rel='noreferrer'
-                  >
-                    +880 1710021512
-                  </a>
-                </p>
-              </div>
-            </div>
-            <div className='col-lg-4'>
-              <div className='border p-4 rounded-3 mb-4'>
-                <span className='fs-1'>
-                  <RiSkypeLine />
-                </span>
-                <h4>Skype</h4>
-                <p className='mb-0 lead'>
-                  <a
-                    className='text-decoration-none text-link link-primary'
-                    href='https://join.skype.com/invite/vldEXJepMI0M'
-                    target='_blank'
-                    rel='noreferrer'
-                  >
-                    Monir Hossain
-                  </a>
-                </p>
-              </div>
-            </div>
-            <div className='col-lg-4'>
-              <div className='border p-4 rounded-3'>
-                <span className='fs-1'>
-                  <RiMessengerFill />
-                </span>
-                <h4>Messanger</h4>
-                <p className='mb-0 lead'>
-                  <a
-                    className='text-decoration-none text-link link-primary'
-                    href='http://m.me/getmonir'
-                    target='_blank'
-                    rel='noreferrer'
-                  >
-                    getmonir
-                  </a>
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-    </>
-  );
+	return (
+		<>
+			<Meta title='Contact Me - Monir Hossain' />
+			<header className='py-10 py-lg-11'>
+				<div className='container'>
+					<div className='row'>
+						<div className='col-lg-7 text-center mx-auto'>
+							<h2 className='display-5 mb-3'>Contact Me</h2>
+							<p className='mb-0 lead'>I &apos;d love to find out more about your upcoming WordPress project. Get in touch today.</p>
+						</div>
+					</div>
+				</div>
+			</header>
+			<section className='pb-10 pb-lg-11'>
+				<div className='container'>
+					<div className='row'>
+						<div className='col-lg-4'>
+							<div className='border p-4 rounded-3 mb-4'>
+								<span className='fs-1'>
+									<RiMailSendLine />
+								</span>
+								<h4>Email</h4>
+								<p className='mb-0 lead'>
+									<a
+										className='text-decoration-none text-link link-primary'
+										href='mailto:monirh.me@gmail.com'
+										target='_blank'
+										rel='noreferrer'
+									>
+										monirh.me@gmail.com
+									</a>
+								</p>
+							</div>
+						</div>
+						<div className='col-lg-4'>
+							<div className='border p-4 rounded-3 mb-4'>
+								<span className='fs-1'>
+									<RiWhatsappLine />
+								</span>
+								<h4>Whatsapp</h4>
+								<p className='mb-0 lead'>
+									<a
+										className='text-decoration-none text-link link-primary'
+										href='https://wa.me/+8801710021512'
+										target='_blank'
+										rel='noreferrer'
+									>
+										+88 01710 021512
+									</a>
+								</p>
+							</div>
+						</div>
+						{/* <div className='col-lg-4'>
+							<div className='border p-4 rounded-3 mb-4'>
+								<span className='fs-1'>
+									<RiSkypeLine />
+								</span>
+								<h4>Skype</h4>
+								<p className='mb-0 lead'>
+									<a
+										className='text-decoration-none text-link link-primary'
+										href='https://join.skype.com/invite/vldEXJepMI0M'
+										target='_blank'
+										rel='noreferrer'
+									>
+										Monir Hossain
+									</a>
+								</p>
+							</div>
+						</div> */}
+						<div className='col-lg-4'>
+							<div className='border p-4 rounded-3'>
+								<span className='fs-1'>
+									<RiMessengerFill />
+								</span>
+								<h4>Messanger</h4>
+								<p className='mb-0 lead'>
+									<a
+										className='text-decoration-none text-link link-primary'
+										href='http://m.me/monirhossain70'
+										target='_blank'
+										rel='noreferrer'
+									>
+										Monir Hossain
+									</a>
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
+		</>
+	);
 };
 
 export default index;
